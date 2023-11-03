edc3ea3f6712a797cf6362ba942fd466a8ebc170
Ego Maragustaf <egomaragustaf@gmail.com>
2023-11-03 20:07:23 +0700

    chore: install card shadcn-ui

---
+++ app/components/ui/card.tsx
@@ -0,0 +1,66 @@
+import * as React from "react";
+
+const Card = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>((props, ref) => (
+  <div
+    ref={ref}
+    className="w-auto lg:w-auto h-full flex flex-col bg-white hover:bg-slate-100 border border-gray-200 rounded-lg shadow"
+    {...props}
+  />
+));
+Card.displayName = "Card";
+
+const CardHeader = React.forwardRef<
+  HTMLImageElement,
+  React.HTMLAttributes<HTMLDivElement>
+>((props, ref) => <div ref={ref} className="w-full mb-4" {...props} />);
+CardHeader.displayName = "CardHeader";
+
+const CardContent = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <div ref={ref} className="flex flex-col gap-1 h-full p-2" {...props} />
+));
+CardContent.displayName = "CardContent";
+
+const CardTitle = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>((props, ref) => (
+  <div
+    ref={ref}
+    className="font-semibold leading-none tracking-tight"
+    {...props}
+  />
+));
+CardTitle.displayName = "CardTitle";
+
+const CardDescription = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>((props, ref) => <div ref={ref} className="text-sm" {...props} />);
+CardDescription.displayName = "CardDescription";
+
+const CardFooter = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>((props, ref) => (
+  <div
+    ref={ref}
+    className="font-bold text-lg text-primary mt-auto flex flex-col gap-1"
+    {...props}
+  />
+));
+CardFooter.displayName = "CardFooter";
+
+export {
+  Card,
+  CardHeader,
+  CardContent,
+  CardTitle,
+  CardDescription,
+  CardFooter,
+};
